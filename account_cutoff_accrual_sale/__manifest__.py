a9a2b624aee903dc0b27edb0e1495927e52a8a79
Jacques-Etienne Baudoux <je@bcim.be>
2023-10-25 15:26:33 +0200

    [ADD] account_cutoff_accrual_sale

---
+++ account_cutoff_accrual_sale/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2018 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Account Cut-off Accrual Sale",
+    "version": "16.0.1.0.0",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "summary": "Accrued Revenue on Sales Order",
+    "author": "BCIM, Odoo Community Association (OCA)",
+    "maintainers": ["jbaudoux"],
+    "website": "https://github.com/OCA/account-closing",
+    "depends": ["account_cutoff_accrual_order_base", "sale"],
+    "data": [
+        "views/account_cutoff.xml",
+        "views/account_cutoff_line.xml",
+        "data/ir_cron.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
