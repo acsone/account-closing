b1c3fb8f2e93855fa0bc7117c5e465c1d3232b2e
Raphaël Valyi <rvalyi@akretion.com>
2025-02-28 00:53:02 +0000

    [LIC] account_invoice_start_end_dates: change to LGPL

--- account_invoice_start_end_dates/__manifest__.py
+++ account_invoice_start_end_dates/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2021 Akretion France (http://www.akretion.com/)
 # Copyright 2018-2021 Camptocamp
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Account Invoice Start End Dates",
