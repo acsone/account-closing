4adae98096c22d8540f7b5e0737ea96f2c0706eb
Pierre Verkest <pierre@verkest.fr>
2026-06-11 17:00:12 +0200

    [MIG] account_cutoff_start_end_dates: get ride of assert in production code

--- account_cutoff_start_end_dates/tests/test_account_cutoff_prepaid.py
+++ account_cutoff_start_end_dates/tests/test_account_cutoff_prepaid.py
@@ -381,3 +381,46 @@ class TestAccountCutoffStartEndDates(AccountCutoffCommon):
         )
         vals = cutoff._prepare_date_cutoff_line(aml, {})
         self.assertNotIn("cutoff_amount", vals)
+
+    def test_forecast_enable_raises_when_not_draft(self):
+        cutoff = self.env["account.cutoff"].create(
+            {
+                "company_id": self.company.id,
+                "cutoff_type": "prepaid_expense",
+            }
+        )
+        cutoff.forecast_enable()
+        with self.assertRaisesRegex(UserError, "enable forecast mode from draft state"):
+            cutoff.forecast_enable()
+
+    def test_forecast_disable_raises_when_not_forecast(self):
+        cutoff = self.env["account.cutoff"].create(
+            {
+                "company_id": self.company.id,
+                "cutoff_type": "prepaid_expense",
+            }
+        )
+        with self.assertRaisesRegex(
+            UserError, "disable forecast mode from forecast state"
+        ):
+            cutoff.forecast_disable()
+
+    def test_prepare_date_cutoff_line_raises_when_invalid_dates(self):
+        cutoff = self.env["account.cutoff"].create(
+            {
+                "company_id": self.company.id,
+                "cutoff_type": "prepaid_expense",
+            }
+        )
+
+        from unittest.mock import Mock
+
+        from odoo import fields
+
+        aml = Mock()
+        aml.id = 999
+        aml.start_date = fields.Date.to_date("2026-06-30")
+        aml.end_date = fields.Date.to_date("2026-06-01")
+
+        with self.assertRaisesRegex(ValidationError, "Total days should always be > 0"):
+            cutoff._prepare_date_cutoff_line(aml, {})
